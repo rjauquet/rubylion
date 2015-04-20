cfc7d18c04d1c33d0f368ad2e46fc94e22a33500
Rob Jauquet <rjauquet@gmail.com>
2015-04-19 23:50:51 -0500

    adding markdown syntax to react

--- js/entry.jsx
+++ js/entry.jsx
@@ -1,13 +1,28 @@
-React = require('react/addons');
+var React   = require('react/addons'),
+    marked  = require('marked');
 
 var Main = React.createClass({
-  render: function() {
-    return (
-      <div className="commentBox">
-        Hello, world! I am a CommentBox.
-      </div>
-    );
-  }
+    getDefaultProps: function (){
+        return {
+            style: {
+                width: '100px',
+                height: 'auto',
+            }
+        };
+    },
+    getInitialState: function (){
+        return {
+            markup: '### Hello, me'
+        };
+    },
+    render: function() {
+        var markup = marked(this.state.markup, {sanitize: true});
+        return (
+            <div className="main" style={this.props.style}>
+                <span dangerouslySetInnerHTML={{__html: markup}} />
+            </div>
+        );
+    }
 });
 
 React.render(<Main />, document.getElementById('content'));
