c88b305c3ee35633dc71eae517e83ec8f63137f1
Rob Jauquet <rjauquet@gmail.com>
2015-04-20 21:41:19 -0500

    adding page requests

--- go/src/back/back.go
+++ go/src/back/back.go
@@ -2,20 +2,58 @@ package main
 
 import (
     "fmt"
-    "html"
     "log"
     "net/http"
+    "io/ioutil"
+    "encoding/json"
 
     "github.com/gorilla/mux"
 )
+
+type Page struct {
+    Text string
+}
 
 func main() {
 
     router := mux.NewRouter().StrictSlash(true)
     router.HandleFunc("/", Index)
+    router.HandleFunc("/pages", getPageNames)
+    router.HandleFunc("/page/{pageName}", getPageByName)
+
     log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+    fmt.Fprintf(w, "Welcome to the rubylion API")
+}
+
+func getPageNames(w http.ResponseWriter, r *http.Request) {
+    var names []string
+    files, _ := ioutil.ReadDir("./pages")
+    for _, f := range files {
+        names = append(names, f.Name())
+    }
+
+    log.Printf("%s", names)
+
+    if err := json.NewEncoder(w).Encode(names); err != nil {
+        panic(err)
+    }
+}
+
+func getPageByName(w http.ResponseWriter, r *http.Request) {
+    files, _ := ioutil.ReadDir("./pages")
+    for _, f := range files {
+        if f.name() == r.pageName {
+            log.Printf("%s", r.pageName)
+            if err := json.NewEncoder(w).Encode(f); err != nil {
+                panic(err)
+            }
+        } else {
+            if err := json.NewEncoder(w).Encode(""); err != nil {
+                panic(err)
+            }
+        }
+    }
 }
